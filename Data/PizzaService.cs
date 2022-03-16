da23f5ca1fbe782b9c30c3150c0be5b50be5ecb9
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-15 23:29:31 -0400

    Made a few changes to add a database and use it

---
+++ Data/PizzaService.cs
@@ -0,0 +1,16 @@
+﻿namespace BlazingPizza.Data
+{
+    public class PizzaService
+    {
+        public async Task<Pizza[]> GetPizzasAsync()
+        {
+            Pizza[] pizza = new Pizza[5];
+            pizza.Append(new Pizza { PizzaId = 1, Name = "Pizza1" });
+            pizza.Append(new Pizza { PizzaId = 2, Name = "Pizza2" });
+            pizza.Append(new Pizza { PizzaId = 3, Name = "Pizza3" });
+            pizza.Append(new Pizza { PizzaId = 4, Name = "Pizza4" });
+            pizza.Append(new Pizza { PizzaId = 5, Name = "Pizza5" });
+            return pizza;
+        }
+    }
+}
