f5abc33292ecb7bd7f1b73334ace644411c0565b
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-17 08:54:01 -0400

    Prepare models for validation

---
+++ Model/Pizzas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazingPizza
+{
+    public class Pizzas
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [EmailAddress]
+        public string ChefEmail { get; set; }
+        [Required]
+        [Range(10.00, 25.00)]
+        public decimal Price { get; set; }
+    }
+}
