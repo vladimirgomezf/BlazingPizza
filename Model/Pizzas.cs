71452b02d67899153ca91c1398db12e1ca42236d
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-17 09:00:02 -0400

    Control your app's form validation

--- Model/Pizzas.cs
+++ Model/Pizzas.cs
@@ -7,13 +7,15 @@ namespace BlazingPizza
     public class Pizzas
     {
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "You must set a name for your pizza.")]
         public string Name { get; set; }
         public string Description { get; set; }
-        [EmailAddress]
+        [EmailAddress(ErrorMessage = "You must set a valid email address for the chef responsible for the pizza recipe.")]
         public string ChefEmail { get; set; }
         [Required]
-        [Range(10.00, 25.00)]
+        [Range(10.00, 25.00, ErrorMessage = "You must set a price between $10 and $25.")]
         public decimal Price { get; set; }
+        [PizzaBase]
+        public string Base { get; set; }
     }
 }
---
+++ PizzaBase.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazingPizza
+{
+    public class PizzaBase : ValidationAttribute
+    {
+        public string GetErrorMessage() => $"Sorry, that's not a valid pizza base.";
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value != "Tomato" || value != "Pesto")
+            {
+                return new ValidationResult(GetErrorMessage());
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
